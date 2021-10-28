2cb90303241b541f0a2e56d4507ef2c1a2a2537e
Andy Reagan <andy@andyreagan.com>
2021-10-28 14:42:18 -0400

    Clean up comments from setup.py.

--- setup.py
+++ setup.py
@@ -31,54 +31,24 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: Unix',
         'Operating System :: POSIX',
-        # 'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        # 'Programming Language :: Python :: 3.8',
-        # 'Programming Language :: Python :: Implementation :: CPython',
-        # 'Programming Language :: Python :: Implementation :: PyPy',
-        # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Utilities',
     ],
     project_urls={
-        # 'Documentation': 'https://mdbtools-util.readthedocs.io/',
-        # 'Changelog': 'https://mdbtools-util.readthedocs.io/en/latest/changelog.html',
         'Issue Tracker': 'https://github.com/andyreagan/mdbtools-util/issues',
     },
-    keywords=[
-        # eg: 'keyword1', 'keyword2', 'keyword3',
-    ],
+    keywords=[],
     python_requires='>=3.6',
-    install_requires=[
-        'click',
-        'requests',
-        'pandas',
-        'Jinja2',
-        'python-dotenv',
-        'vertica_python',
-        'xlsxwriter'
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
-    ],
-    extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
-    },
-    # entry_points={
-    #     'console_scripts': [
-    #         'mdbtools-util = mdbtools_util.cli:main',
-    #     ]
-    # },
+    install_requires=[],
+    extras_require={},
 )
